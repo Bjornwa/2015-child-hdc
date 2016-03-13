825701f132bc7743052272b4eaf5c011fc5d5838
Björn Wallin <bjorn.a.wallin@gmail.com>
2016-03-13 22:06:23 +0100

    Upphovsrätt
    "Text och bilder är skyddade av lagen om upphovsrätt..."

--- footer.php
+++ footer.php
@@ -14,15 +14,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php
-				/**
-				 * Fires before the Twenty Fifteen footer text for footer customization.
-				 *
-				 * @since Twenty Fifteen 1.0
-				 */
-				do_action( 'twentyfifteen_credits' );
-			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+			<a>Text och bilder är skyddade av lagen om upphovsrätt. All kopiering eller publicering utan tillstånd är förbjuden.</a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
