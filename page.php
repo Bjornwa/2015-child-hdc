58f72bd056b71a0e717c30eb3283e38626397704
Björn Wallin <bjorn.a.wallin@gmail.com>
2016-04-22 08:41:18 +0200

    Get logo img link working

--- page.php
+++ page.php
@@ -15,8 +15,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<img style="width: 12%;position: absolute;z-index: 2;top: 1em;left: 1.2em;" 
-		src="wp-content/uploads/2015/05/cropped-HBG_Drumcorps_RGB_scale-300x300.jpg" 
+		<img style="width: 10%;position: absolute;z-index: 2;top: 1em;left: 1.2em;" 
+		src="/wp-content/uploads/2015/05/cropped-HBG_Drumcorps_RGB_scale-300x300.jpg"
+		href="/"
 		id="single-logo">
 
 		<?php
